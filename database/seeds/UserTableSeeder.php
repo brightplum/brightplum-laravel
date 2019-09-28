d46fdaa97b93c1922200f365388c70cacf7ec76c
Gustavo Ramírez Fallas <gustavo.r@myplanet.com>
2019-09-28 15:42:48 -0600

    Initial commit with laravel project

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = 'Gustavo Ramirez';
+        $user->email = 'gustavo@brightplum.com';
+        $user->password = bcrypt('gustavo123');
+        $user->save();
+        $user->roles()->attach(Role::where('name', 'admin')->first());
+
+        $admin = new User;
+        $admin->name = 'Fredric Mitchell';
+        $admin->email = 'fredric@brightplum.com';
+        $admin->password = bcrypt('12345678');
+        $admin->save();
+        $admin->roles()->attach(Role::where('name', 'user')->first());
+    }
+}
